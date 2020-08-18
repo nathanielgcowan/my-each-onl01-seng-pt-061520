df790352482b3a0469814b8a8aee32ad535cd1d9
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-18 02:02:21 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 def my_each(array)
   i = 0
@@ -7,3 +8,13 @@ def my_each(array)
   end
   array
 end
+=======
+def my_each # put argument(s) here
+ collection=[1,2,3,4]
+ while my_each(words) do |item|
+      "word"
+  counter+=1
+
+  end
+end
+>>>>>>> f35b8fbda252ee2ae48c7e2ea15451da90ee5f4c
